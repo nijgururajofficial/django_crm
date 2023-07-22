147cc41aaf122220fc2df034f5c5a1d0b8ec7ea5
nijgururajofficial <nijgururaja12@gmail.com>
2023-07-22 15:39:06 +0530

    final

--- crm_app/views.py
+++ crm_app/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django. contrib import messages
-from .forms import SignUpForm
+from .forms import SignUpForm, AddRecordForm
 from .models import Record
 
 # Create your views here.
@@ -16,7 +16,7 @@ def home(request):
         # authenticate
         user = authenticate(request, username=username, password=password)
         if user is not None:
-            login(request, user)
+            login(  request, user)
             messages.success(request, "You have logged in successfully")
             return redirect('home')
         else:
@@ -64,6 +64,32 @@ def delete_record(request, pk):
         delete_record.delete()
         messages.success(request, "Record Deleted Successfully...")
         return redirect('home')
+    else:
+        messages.success(request, "You must be logged in to view this page...")
+        return redirect('home')
+    
+def add_record(request):
+    form = AddRecordForm(request.POST or None)
+    if request.user.is_authenticated:
+        if request.method == "POST":
+            if form.is_valid():
+                form.save()
+                messages.success(request, "Record Added...")
+                return redirect('home')
+        return render(request, "add_record.html", {'form':form})
+    else:
+        messages.success(request, "You must be logged in to view this page...")
+        return redirect('home')
+
+def update_record(request,pk):
+    if request.user.is_authenticated:
+        customer_record = Record.objects.get(id=pk)
+        form = AddRecordForm(request.POST or None, instance=customer_record)
+        if form.is_valid():
+            form.save()
+            messages.success(request,"Record has been updated")
+            return redirect('home')
+        return render(request, "update_record.html",{'form':form})
     else:
         messages.success(request, "You must be logged in to view this page...")
         return redirect('home')
