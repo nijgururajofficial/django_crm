f6f806c36acdf69f5b3579ef2f68c3620e49b6a9
nijgururajofficial <nijgururaja12@gmail.com>
2023-07-22 12:25:32 +0530

    individual_records

--- crm_app/views.py
+++ crm_app/views.py
@@ -47,5 +47,23 @@ def register_user(request):
     else:
         form = SignUpForm
         return render(request, "register.html", {'form':form})
+    return render(request, "register.html", {'form':form})
     
-            
+
+def customer_record(request, pk):
+    if request.user.is_authenticated:
+        customer_record = Record.objects.get(id=pk)
+        return render(request, "record.html", {'customer_record':customer_record})
+    else:
+        messages.success(request, "You must be logged in to view this page...")
+        return redirect('home')
+    
+def delete_record(request, pk):
+    if request.user.is_authenticated:
+        delete_record = Record.objects.get(id=pk)
+        delete_record.delete()
+        messages.success(request, "Record Deleted Successfully...")
+        return redirect('home')
+    else:
+        messages.success(request, "You must be logged in to view this page...")
+        return redirect('home')
