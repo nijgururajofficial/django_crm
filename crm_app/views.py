58e56d3a2a47b658d69c23a2a91953553d120b30
nijgururajofficial <nijgururaja12@gmail.com>
2023-07-22 11:09:06 +0530

    records

--- crm_app/views.py
+++ crm_app/views.py
@@ -2,9 +2,12 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django. contrib import messages
 from .forms import SignUpForm
+from .models import Record
 
 # Create your views here.
 def home(request):
+    records = Record.objects.all()
+
     # check if user is authenticated
     if request.method == 'POST':
         username = request.POST['username']
@@ -20,7 +23,7 @@ def home(request):
             messages.success(request, "There was an error logging in. Please try again...")
             return redirect('home')
     else:
-        return render(request, "home.html", {})
+        return render(request, "home.html", {'records':records})
         
 
 
